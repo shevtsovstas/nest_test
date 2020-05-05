6b69ae9cd9d476c3fe93583bc1416f3e224f11f9
shevtsovstas <shevtsovstas@gmail.com>
2020-05-05 17:37:27 +0300

    Swagger docs implemented

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
+
+  const options = new DocumentBuilder()
+    .setTitle('Todo swagger docs')
+    .setDescription('Todo API description')
+    .setVersion('1.0')
+    .addTag('todo list')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/todo/dto.ts
+++ src/modules/todo/dto.ts
@@ -1,9 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateDto {
+  @ApiProperty()
   title: string
+  @ApiProperty({required:false})
   isCompleted?: boolean
 }
 
 export class UpdateDto {
+  @ApiProperty()
   title: string
+  @ApiProperty({required:false})
   isCompleted?: boolean
 }
